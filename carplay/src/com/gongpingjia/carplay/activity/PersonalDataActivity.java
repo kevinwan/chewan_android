8913af1a5998ac53b3be4ed856a33c14e5c86675
wangshuo5945 <308852453@qq.com>
2015-07-16 09:20:09 +0800

    更新代码
    设置    编辑资料界面 布局

---
+++ carplay/src/com/gongpingjia/carplay/activity/PersonalDataActivity.java
@@ -0,0 +1,74 @@
+package com.gongpingjia.carplay.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.gongpingjia.carplay.R;
+
+/**
+ * 编辑资料
+ * 
+ * @author Administrator
+ * 
+ */
+public class PersonalDataActivity extends CarPlayBaseActivity implements
+		OnClickListener {
+	RelativeLayout personal_city, personal_driving_years, personal_head,
+			personal_nickname, personal_sex;
+	ImageView head_img;
+	TextView nickname_txt, sex_txt, driving_years_txt, city_txt, title;
+	private PersonalDataActivity mySelf = this;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.personal_data);
+		personal_city = (RelativeLayout) findViewById(R.id.personal_city);
+		personal_driving_years = (RelativeLayout) findViewById(R.id.personal_driving_years);
+		personal_head = (RelativeLayout) findViewById(R.id.personal_head);
+		personal_nickname = (RelativeLayout) findViewById(R.id.personal_nickname);
+		personal_sex = (RelativeLayout) findViewById(R.id.personal_sex);
+		head_img = (ImageView) findViewById(R.id.head_img);
+		nickname_txt = (TextView) findViewById(R.id.nickname_txt);
+		sex_txt = (TextView) findViewById(R.id.sex_txt);
+		driving_years_txt = (TextView) findViewById(R.id.driving_years_txt);
+		city_txt = (TextView) findViewById(R.id.city_txt);
+		title = (TextView) findViewById(R.id.title);
+		title.setText("编辑资料");
+		personal_city.setOnClickListener(this);
+		personal_driving_years.setOnClickListener(this);
+		personal_head.setOnClickListener(this);
+		personal_nickname.setOnClickListener(this);
+		personal_sex.setOnClickListener(this);
+
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		switch (arg0.getId()) {
+		case R.id.personal_head:
+
+			break;
+		case R.id.personal_driving_years:
+
+			break;
+		case R.id.personal_nickname:
+
+			break;
+		case R.id.personal_sex:
+
+			break;
+		case R.id.personal_city:
+
+			break;
+
+		default:
+			break;
+		}
+	}
+
+}
---
+++ carplay/src/com/gongpingjia/carplay/activity/SettingActivity.java
@@ -0,0 +1,71 @@
+package com.gongpingjia.carplay.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.gongpingjia.carplay.R;
+
+public class SettingActivity extends CarPlayBaseActivity implements
+		OnClickListener {
+	Button setting_btn;
+	RelativeLayout setting_clear, setting_like, setting_about_us,
+			setting_versions, setting_at_versions;
+	TextView title;
+	  private SettingActivity mySelf = this;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_setting);
+		setting_clear = (RelativeLayout) findViewById(R.id.setting_clear);
+		setting_like = (RelativeLayout) findViewById(R.id.setting_like);
+		setting_about_us = (RelativeLayout) findViewById(R.id.setting_about_us);
+		setting_versions = (RelativeLayout) findViewById(R.id.setting_versions);
+		setting_at_versions = (RelativeLayout) findViewById(R.id.setting_at_versions);
+		setting_btn = (Button) findViewById(R.id.setting_quit);
+		title = (TextView) findViewById(R.id.title);
+		title.setText("设置");
+		setting_clear.setOnClickListener(this);
+		setting_like.setOnClickListener(this);
+		setting_about_us.setOnClickListener(this);
+		setting_versions.setOnClickListener(this);
+		setting_at_versions.setOnClickListener(this);
+		setting_btn.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		switch (arg0.getId()) {
+		case R.id.setting_clear:
+			Toast.makeText(mySelf, "清理缓存",Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.setting_like:
+			Toast.makeText(mySelf, "喜欢鼓励",Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.setting_about_us:
+			Toast.makeText(mySelf, "关于我们",Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.setting_versions:
+			Toast.makeText(mySelf, "版本介绍",Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.setting_at_versions:
+			Toast.makeText(mySelf, "当前版本",Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.setting_quit:
+			Toast.makeText(mySelf, "退出登录",Toast.LENGTH_SHORT).show();
+			Intent intent = new Intent(mySelf,PersonalDataActivity.class);
+			startActivity(intent);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+}
