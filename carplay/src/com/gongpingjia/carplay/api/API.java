0d7313aecb72bb9f6d4ebe215af16619f96fa090
qibutian <603986018@qq.com>
2015-07-17 09:35:00 +0800

    增加测试接口
    增加测试接口

---
+++ carplay/src/com/gongpingjia/carplay/api/API.java
@@ -0,0 +1,8 @@
+package com.gongpingjia.carplay.api;
+
+public class API
+{
+    public static final String Baseurl = "http://api7.gongpingjia.com/";
+    
+    public static final String allCarData = Baseurl + "/mobile/buy/car/search/";
+}
