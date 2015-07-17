064a31c9037ccf4a2f58164a9a7d31d752e23aaf
hu845643018 <845643018@qq.com>
2015-07-17 16:02:18 +0800

    更新布局
    更新布局

---
+++ carplay/src/com/gongpingjia/carplay/activity/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.gongpingjia.carplay.activity;
+
+import com.gongpingjia.carplay.R;
+import com.gongpingjia.carplay.R.id;
+import com.gongpingjia.carplay.R.layout;
+import com.gongpingjia.carplay.R.menu;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * 登录页面
+ * @author Administrator
+ *
+ */
+public class LoginActivity extends CarPlayBaseActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+
+
+	@Override
+	public void initView() {
+		// TODO Auto-generated method stub
+		
+	}
+}
