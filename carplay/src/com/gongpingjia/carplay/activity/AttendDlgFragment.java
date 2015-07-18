a5289b9b6f6412cd4125cf5d784a60a18bde7491
zlidentify <zlidentify@gmail.com>
2015-07-18 14:43:18 +0800

    聊天界面及简单实现

--- carplay/src/com/gongpingjia/carplay/activity/AttendDlgFragment.java
+++ carplay/src/com/gongpingjia/carplay/activity/AttendDlgFragment.java
@@ -10,12 +10,19 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 
-
 /*
  *@author zhanglong
  *Email:1269521147@qq.com
  */
 public class AttendDlgFragment extends DialogFragment {
+
+    private OnDialogCallbacks mCallbacks;
+
+    static AttendDlgFragment newInstance(OnDialogCallbacks callbacks) {
+        AttendDlgFragment f = new AttendDlgFragment();
+        f.mCallbacks = callbacks;
+        return f;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,17 +31,17 @@ public class AttendDlgFragment extends DialogFragment {
         View view = inflater.inflate(R.layout.fragment_dlg_attend, container);
         Button submitBtn = (Button) view.findViewById(R.id.btn_submit);
         submitBtn.setOnClickListener(new View.OnClickListener() {
-            
+
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                
+                mCallbacks.onSubmit();
             }
         });
         return view;
     }
-    
-    public interface OnDialogCallbacks{
+
+    public interface OnDialogCallbacks {
         void onSubmit();
     }
 
---
+++ carplay/src/com/gongpingjia/carplay/bean/ChatMessage.java
@@ -0,0 +1,39 @@
+package com.gongpingjia.carplay.bean;
+
+/*
+ *@author zhanglong
+ *Email:1269521147@qq.com
+ */
+public class ChatMessage {
+
+    private String type;
+
+    private String time;
+
+    private String msg;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+}
