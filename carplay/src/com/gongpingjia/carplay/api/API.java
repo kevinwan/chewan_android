25c011f7afa343a105e44a0df6b033178f1da97d
hu845643018 <845643018@qq.com>
2015-07-21 15:47:38 +0800

    更新代码 新增注册功能
    更新代码 新增注册功能

--- carplay/src/com/gongpingjia/carplay/api/API.java
+++ carplay/src/com/gongpingjia/carplay/api/API.java
@@ -5,4 +5,8 @@ public class API
     public static final String Baseurl = "http://api7.gongpingjia.com/";
     
     public static final String allCarData = Baseurl + "/mobile/buy/car/search/";
+    
+    public static final String CWBaseurl = "http://cwapi.gongpingjia.com/v1";
+    
+    
 }
