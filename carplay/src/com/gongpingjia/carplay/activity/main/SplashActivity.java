e3c8532d8d9dcc4767bd21fb1ff1a990b592c552
hu845643018 <845643018@qq.com>
2015-07-24 11:24:13 +0800

    启动页
    启动页

--- carplay/src/com/gongpingjia/carplay/activity/main/SplashActivity.java
+++ carplay/src/com/gongpingjia/carplay/activity/main/SplashActivity.java
@@ -1,30 +1,18 @@
 package com.gongpingjia.carplay.activity.main;
 
+import net.duohuo.dhroid.ioc.IocContainer;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
-import android.support.v4.view.ViewPager.OnPageChangeListener;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.Button;
+import android.os.Handler;
 
 import com.gongpingjia.carplay.R;
 import com.gongpingjia.carplay.activity.CarPlayBaseActivity;
-import com.gongpingjia.carplay.adapter.SimplePageAdapter;
+import com.gongpingjia.carplay.util.CarPlayPerference;
 
-/**
- * 引导页 hqh
- * 
- * @author Administrator
- * 
- */
 public class SplashActivity extends CarPlayBaseActivity {
-	SimplePageAdapter pagerAdapter;
-	ViewPager pager;
-	View firstView, secondView,thirdView;
-	LayoutInflater mLayoutInflater;
-	//开始畅游
-	Button btn_start;
+	
+	CarPlayPerference per;
+	private final Handler mHandler = new Handler();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -33,45 +21,44 @@ public class SplashActivity extends CarPlayBaseActivity {
 
 	@Override
 	public void initView() {
-		mLayoutInflater = LayoutInflater.from(self);
-		firstView = mLayoutInflater.inflate(R.layout.vp_first, null);
-		secondView = mLayoutInflater.inflate(R.layout.vp_second, null);
-		thirdView = mLayoutInflater.inflate(R.layout.vp_third, null);
-		pager = (ViewPager) findViewById(R.id.viewpager);
-		pagerAdapter = new SimplePageAdapter(firstView, secondView,thirdView);
+		per = IocContainer.getShare().get(CarPlayPerference.class);
+		per.load();
 		
-		 pager.setAdapter(pagerAdapter);
-	        pager.setOnPageChangeListener(new OnPageChangeListener() {
-				
-				@Override
-				public void onPageSelected(int arg0) {
-					// TODO Auto-generated method stub
+				if (per.isFirst==0) {
 					
-				}
-				
-				@Override
-				public void onPageScrolled(int arg0, float arg1, int arg2) {
-					// TODO Auto-generated method stub
+					first();
 					
+				}else{
+					notFirst();
 				}
-				
-				@Override
-				public void onPageScrollStateChanged(int arg0) {
-					// TODO Auto-generated method stub
-					
-				}
-			});
-	        
-	        btn_start=(Button) thirdView.findViewById(R.id.start);
-	        
-	        btn_start.setOnClickListener(new View.OnClickListener() {
-				
-				@Override
-				public void onClick(View arg0) {
-					Intent intent=new Intent(self,MainActivity.class);
+		
+	}
+
+	private void first() {
+		 mHandler.postDelayed(new Runnable()
+	        {
+	            @Override
+	            public void run()
+	            {
+	            	Intent intent=new Intent(self,GuidanceActivity.class);
+					startActivity(intent);
+					per.isFirst=1;
+					per.commit();
+					self.finish();
+	            }
+	        }, 3000);
+	}
+	
+	private void notFirst() {
+		 mHandler.postDelayed(new Runnable()
+	        {
+	            @Override
+	            public void run()
+	            {
+	            	Intent intent=new Intent(self,MainActivity.class);
 					startActivity(intent);
 					self.finish();
-				}
-			});
+	            }
+	        }, 3000);
 	}
 }
