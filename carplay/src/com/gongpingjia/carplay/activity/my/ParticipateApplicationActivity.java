c62f2e23f719c2bab885f73e5951dcadf492c270
wangshuo5945 <308852453@qq.com>
2015-07-24 13:53:31 +0800

    提交代码
    提交代码

--- carplay/src/com/gongpingjia/carplay/activity/my/ParticipateApplicationActivity.java
+++ carplay/src/com/gongpingjia/carplay/activity/my/ParticipateApplicationActivity.java
@@ -1,23 +1,21 @@
 package com.gongpingjia.carplay.activity.my;
 
+import net.duohuo.dhroid.adapter.BeanAdapter;
 import net.duohuo.dhroid.adapter.FieldMap;
 import net.duohuo.dhroid.adapter.NetJSONAdapter;
-import net.duohuo.dhroid.util.ViewUtil;
+import net.duohuo.dhroid.net.DhNet;
+import net.duohuo.dhroid.net.NetTask;
+import net.duohuo.dhroid.net.Response;
 import net.duohuo.dhroid.view.NetRefreshAndMoreListView;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.gongpingjia.carplay.R;
-import com.gongpingjia.carplay.R.id;
-import com.gongpingjia.carplay.R.layout;
 import com.gongpingjia.carplay.activity.CarPlayBaseActivity;
-import com.gongpingjia.carplay.api.API;
 import com.gongpingjia.carplay.bean.User;
-
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 /**
  * 活动参与申请
@@ -30,6 +28,7 @@ public class ParticipateApplicationActivity extends CarPlayBaseActivity {
 
 	NetRefreshAndMoreListView listView;
 	RelativeLayout participate_sex;
+	private String strApplicationId;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -70,7 +69,7 @@ public class ParticipateApplicationActivity extends CarPlayBaseActivity {
 
 			@Override
 			public Object fix(View itemV, Integer position, Object o, Object jo) {
-				String strApplicationId = o.toString();
+				strApplicationId = o.toString();
 
 				return null;
 			}
@@ -94,6 +93,37 @@ public class ParticipateApplicationActivity extends CarPlayBaseActivity {
 		});
 
 		listView.setAdapter(adapter);
+//		adapter.setOnInViewClickListener(R.id.button_agree,
+//				new BeanAdapter.InViewClickListener() {
+//
+//					@Override
+//					public void OnClickListener(View parentV, View v,
+//							Integer position, Object values) {
+//						final TextView textView = (TextView) v
+//								.findViewById(R.id.button_agree);
+//						DhNet net = new DhNet(
+//								"http://cwapi.gongpingjia.com/v1/application/"
+//										+ strApplicationId
+//										+ "/process?userId=846de312-306c-4916-91c1-a5e69b158014&token=750dd49c-6129-4a9a-9558-27fa74fc4ce7");
+//						net.addParam("action", 1);
+//						net.doPost(new NetTask(self) {
+//
+//							@Override
+//							public void doInUI(Response response,
+//									Integer transfer) {
+//								if (response.isSuccess()) {
+//									textView.setText("已同意");
+//									textView.setBackgroundResource(Color.rgb(
+//											0xf5, 0xf7, 0xfa));
+//									adapter.refresh();
+//								} else {
+//									showToast(response.result);
+//								}
+//
+//							}
+//						});
+//					}
+//				});
 		adapter.showNextInDialog();
 	}
 }
