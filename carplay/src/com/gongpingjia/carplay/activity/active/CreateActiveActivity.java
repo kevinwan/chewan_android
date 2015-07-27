b3405ded17129102a8c46c84ea5ec662d8d8b5ed
zlidentify <zlidentify@gmail.com>
2015-07-27 13:44:29 +0800

    创建活动完成

--- carplay/src/com/gongpingjia/carplay/activity/active/CreateActiveActivity.java
+++ carplay/src/com/gongpingjia/carplay/activity/active/CreateActiveActivity.java
@@ -3,6 +3,7 @@ package com.gongpingjia.carplay.activity.active;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import net.duohuo.dhroid.net.DhNet;
 import net.duohuo.dhroid.net.JSONUtil;
@@ -11,12 +12,14 @@ import net.duohuo.dhroid.net.Response;
 import net.duohuo.dhroid.net.upload.FileInfo;
 import net.duohuo.dhroid.util.PhotoUtil;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
@@ -86,6 +89,13 @@ public class CreateActiveActivity extends CarPlayBaseActivity implements OnClick
 
     private String mCity;
 
+    private String mLocation;
+
+    // 开始时间默认为当前的时间戳
+    private long mStartTimeStamp = System.currentTimeMillis();
+
+    private long mEndTimeStamp = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -101,16 +111,16 @@ public class CreateActiveActivity extends CarPlayBaseActivity implements OnClick
         mFeeOptions.add("我请客");
         mFeeOptions.add("请我吧");
 
-        mTypeOptions.add("看电影啊");
-        mTypeOptions.add("唱歌啊");
-        mTypeOptions.add("跑步啊");
-        mTypeOptions.add("爬山啊");
-        mTypeOptions.add("吃饭啊");
+        mTypeOptions.add("看电影");
+        mTypeOptions.add("吃饭");
+        mTypeOptions.add("唱歌");
+        mTypeOptions.add("旅行");
+        mTypeOptions.add("运动");
+        mTypeOptions.add("拼车");
+        mTypeOptions.add("代驾");
 
         mSeatOptions.add("1");
         mSeatOptions.add("2");
-        mSeatOptions.add("3");
-        mSeatOptions.add("4");
 
         mTypeText = (TextView) findViewById(R.id.tv_active_type);
         mDescriptionText = (TextView) findViewById(R.id.tv_description);
@@ -168,6 +178,9 @@ public class CreateActiveActivity extends CarPlayBaseActivity implements OnClick
                     PhotoUtil.getPhoto(self, Constant.TAKE_PHOTO, Constant.PICK_PHOTO, new File(mCurPath));
                 } else {
                     mPhotoStates.remove(position);
+                    if (mPhotoStates.size() == 8) {
+                        mPhotoStates.add(mLastPhoto);
+                    }
                     mImageAdapter.notifyDataSetChanged();
                 }
             }
@@ -241,6 +254,7 @@ public class CreateActiveActivity extends CarPlayBaseActivity implements OnClick
                 public void result(String date, long datetime, int year, int month, int day) {
                     // TODO Auto-generated method stub
                     mStartTimeText.setText(date);
+                    mStartTimeStamp = datetime;
                 }
             });
             date.show(self);
@@ -253,6 +267,7 @@ public class CreateActiveActivity extends CarPlayBaseActivity implements OnClick
                 public void result(String date, long datetime, int year, int month, int day) {
                     // TODO Auto-generated method stub
                     mEndTimeText.setText(date);
+                    mEndTimeStamp = datetime;
                 }
             });
             date.show(self);
@@ -283,41 +298,39 @@ public class CreateActiveActivity extends CarPlayBaseActivity implements OnClick
             break;
 
         case R.id.btn_finish:
-
             if (mTypeText.getText().toString().equals("")) {
                 showToast("请选择活动");
                 return;
             }
-
             if (mDestimationText.getText().toString().length() == 0) {
                 showToast("请选择目的地");
-                return;
-            }
-
-            if (mStartTimeText.getText().toString().length() == 0) {
-                showToast("请选择开始日期");
-                return;
-            }
-            if (mEndTimeText.getText().toString().length() == 0) {
-                showToast("请选择结束日期");
                 return;
             }
             if (mSeatText.getText().toString().length() == 0) {
                 showToast("请提供座位数");
                 return;
             }
+
             User user = User.getInstance();
-            mDhNet = new DhNet("http://cwapi.gongpingjia.com/v1/activity/register?userId=" + user.getUserId()
-                    + "&token=" + user.getToken());
+            mDhNet = new DhNet(API.createActive + "userId=" + user.getUserId() + "&token=" + user.getToken());
             mDhNet.addParam("type", mTypeText.getText().toString());
             mDhNet.addParam("introduction", mDescriptionText.getText().toString());
-            mDhNet.addParam("start", mStartTimeText.getText().toString());
-            mDhNet.addParam("cover", "[\"68cbd3b0-d19b-4754-8b08-835b9d94a869\"]");
-            mDhNet.addParam("location", mDestimationText.getText().toString());
+            JSONArray array = new JSONArray(mPicIds);
+            mDhNet.addParam("cover", array);
+            mDhNet.addParam("location", mLocation);
             mDhNet.addParam("city", mCity);
+            mDhNet.addParam("address", mDestimationText.getText().toString());
+            mDhNet.addParam("start", mStartTimeStamp);
             mDhNet.addParam("pay", mFeeText.getText().toString());
             mDhNet.addParam("seat", mSeatText.getText().toString());
+            if (mEndTimeStamp != 0) {
+                mDhNet.addParam("end", mEndTimeStamp);
+            }
 
+            Map<String, Object> params = mDhNet.getParams();
+            for (String key : params.keySet()) {
+                Log.e("tag", key + ": " + params.get(key));
+            }
             mDhNet.doPostInDialog(new NetTask(this) {
 
                 @Override
@@ -325,6 +338,13 @@ public class CreateActiveActivity extends CarPlayBaseActivity implements OnClick
                     // TODO Auto-generated method stub
                     if (response.isSuccess()) {
                         showToast("发布成功");
+                    } else {
+                        try {
+                            Log.e("err", response.jSON().getString("errmsg"));
+                        } catch (JSONException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
                     }
                 }
             });
@@ -349,6 +369,7 @@ public class CreateActiveActivity extends CarPlayBaseActivity implements OnClick
             case REQUEST_DESTINATION:
                 mDestimationText.setText(data.getStringExtra("destination"));
                 mCity = data.getStringExtra("city");
+                mLocation = data.getStringExtra("location");
                 break;
             case Constant.TAKE_PHOTO:
                 PhotoUtil.onPhotoFromCamera(self, Constant.ZOOM_PIC, mCurPath, 1, 1, 1000);
@@ -372,7 +393,7 @@ public class CreateActiveActivity extends CarPlayBaseActivity implements OnClick
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
-                            showToast("上传成功");
+                            showToast("图片上传成功");
                         } else {
                             try {
                                 showToast(response.jSON().getString("errmsg"));
--- carplay/src/com/gongpingjia/carplay/view/dialog/DateDialog.java
+++ carplay/src/com/gongpingjia/carplay/view/dialog/DateDialog.java
@@ -15,23 +15,21 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.LinearLayout;
 
-public class DateDialog
-{
+public class DateDialog {
     DatePicker datePicker;
-    
+
     long bendi;
-    
+
     LinearLayout timeLayout;
-    
+
     String time;
-    
+
     OnDateResultListener onDateResultListener;
-    
-    public void show(final Context context)
-    {
+
+    public void show(final Context context) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         View view = View.inflate(context, R.layout.time_dialog, null);
-        datePicker = (DatePicker)view.findViewById(R.id.date_picker);
+        datePicker = (DatePicker) view.findViewById(R.id.date_picker);
         // datePicker.setMaxDate(maxDate)
         bendi = System.currentTimeMillis();
         // datePicker.setMaxDate(bendi+86400000*3);
@@ -40,68 +38,50 @@ public class DateDialog
         cal.setTimeInMillis(bendi);
         datePicker.init(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), null);
         builder.setTitle("选取时间");
-        builder.setPositiveButton("确  定", new DialogInterface.OnClickListener()
-        {
-            
+        builder.setPositiveButton("确  定", new DialogInterface.OnClickListener() {
+
             @Override
-            public void onClick(DialogInterface dialog, int which)
-            {
+            public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
-                
-                String dateString =
-                    new StringBuilder().append(datePicker.getYear())
-                        .append("年")
-                        .append(datePicker.getMonth() + 1)
-                        .append("月")
-                        .append(datePicker.getDayOfMonth())
-                        .append("日")
+
+                String dateString = new StringBuilder().append(datePicker.getYear()).append("年")
+                        .append(datePicker.getMonth() + 1).append("月").append(datePicker.getDayOfMonth()).append("日")
                         .toString();
-                
-                String datef =
-                    new StringBuilder().append(datePicker.getYear())
-                        .append("-")
-                        .append(datePicker.getMonth() + 1)
-                        .append("-")
-                        .append(datePicker.getDayOfMonth())
-                        .append(" ")
+
+                String datef = new StringBuilder().append(datePicker.getYear()).append("-")
+                        .append(datePicker.getMonth() + 1).append("-").append(datePicker.getDayOfMonth()).append(" ")
                         .toString();
-                
+
                 SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-                
+
                 Date date = null;
-                try
-                {
+                try {
                     date = df.parse(datef);
-                }
-                catch (ParseException e)
-                {
+                } catch (ParseException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
                 long time = date.getTime();
-                
-                if (onDateResultListener != null)
-                {
-                    onDateResultListener.result(dateString, time,datePicker.getYear(),datePicker.getMonth()+1,datePicker.getDayOfMonth());
+
+                if (onDateResultListener != null) {
+                    onDateResultListener.result(dateString, time, datePicker.getYear(), datePicker.getMonth() + 1,
+                            datePicker.getDayOfMonth());
                 }
             }
         });
         Dialog dialog = builder.create();
         dialog.show();
     }
-    
-    public OnDateResultListener getOnDateResultListener()
-    {
+
+    public OnDateResultListener getOnDateResultListener() {
         return onDateResultListener;
     }
-    
-    public void setOnDateResultListener(OnDateResultListener onDateResultListener)
-    {
+
+    public void setOnDateResultListener(OnDateResultListener onDateResultListener) {
         this.onDateResultListener = onDateResultListener;
     }
-    
-    public interface OnDateResultListener
-    {
+
+    public interface OnDateResultListener {
         void result(String date, long datetime, int year, int month, int day);
     }
 }
