d276cc59d4749b913e805bfc1813c58f98056b3b
zlidentify <zlidentify@gmail.com>
2015-08-05 17:48:48 +0800

    update msg

--- carplay/src/com/gongpingjia/carplay/activity/msg/MsgFragment.java
+++ carplay/src/com/gongpingjia/carplay/activity/msg/MsgFragment.java
@@ -22,6 +22,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.gongpingjia.carplay.R;
+import com.gongpingjia.carplay.adapter.FragmentMsgAdapter;
 import com.gongpingjia.carplay.api.API;
 import com.gongpingjia.carplay.bean.User;
 import com.gongpingjia.carplay.view.BadgeView;
@@ -38,11 +39,13 @@ public class MsgFragment extends Fragment {
 
     private RefreshAndMoreListView mRefreshListView;
 
-    BadgeView normalMsgBadgeT, applicationMsgBadgeT;
-
-    TextView applicationmsg_contentT, normsg_contentT;
-
-    View aaplication_layoutV, normsg_layoutV;
+    //
+    // BadgeView normalMsgBadgeT, applicationMsgBadgeT;
+    //
+    // TextView applicationmsg_contentT, normsg_contentT;
+    //
+    // View aaplication_layoutV, normsg_layoutV;
+    private FragmentMsgAdapter mAdapter;
 
     Timer mTimer;
 
@@ -56,65 +59,21 @@ public class MsgFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.information, container, false);
-        RelativeLayout leave_comments_layout = (RelativeLayout) view.findViewById(R.id.leave_comments_layout);
 
-//        mRefreshListView = (RefreshAndMoreListView) view.findViewById(R.id.refreshLv_msg);
-//        mRefreshListView.setonRefreshListener(new OnRefreshListener() {
-//
-//            @Override
-//            public void onRefresh() {
-//                getMsgCount();
-//            }
-//        });
-        leave_comments_layout.setOnClickListener(new View.OnClickListener() {
+        mAdapter = new FragmentMsgAdapter(getActivity());
+        final View view = inflater.inflate(R.layout.fragment_msg, container, false);
+        // RelativeLayout leave_comments_layout = (RelativeLayout)
+        // view.findViewById(R.id.leave_comments_layout);
+
+        mRefreshListView = (RefreshAndMoreListView) view.findViewById(R.id.refreshLv_msg);
+        mRefreshListView.setAdapter(mAdapter);
+        mRefreshListView.setonRefreshListener(new OnRefreshListener() {
 
             @Override
-            public void onClick(View arg0) {
-                Intent intent = new Intent(getActivity(), NewMessageActivity.class);
-                intent.putExtra("type", "comment");
-                startActivity(intent);
-
+            public void onRefresh() {
+                getMsgCount();
             }
         });
-        RelativeLayout apply_for_layout = (RelativeLayout) view.findViewById(R.id.apply_for_layout);
-        apply_for_layout.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-                Intent intent = new Intent(getActivity(), NewMessageActivity.class);
-                intent.putExtra("type", "application");
-                startActivity(intent);
-            }
-        });
-        int bgcolor = getActivity().getResources().getColor(R.color.text_orange);
-        applicationmsg_contentT = (TextView) view.findViewById(R.id.applicationmsg_content);
-        normsg_contentT = (TextView) view.findViewById(R.id.normsg_content);
-        normalMsgBadgeT = (BadgeView) view.findViewById(R.id.normal_msg_cout);
-        normalMsgBadgeT.setBadgeBackgroundColor(bgcolor);
-        applicationMsgBadgeT = (BadgeView) view.findViewById(R.id.application_msg_cout);
-        applicationMsgBadgeT.setBadgeBackgroundColor(bgcolor);
-        // aaplication_layoutV = view.findViewById(R.id.applicationmsg_layout);
-        // normsg_layoutV = view.findViewById(R.id.normsg_layout);
-
-        // normalMsgBadgeT = new BadgeView(getActivity(), normsg_contentT);//
-        // 创建一个BadgeView对象，view为你需要显示提醒信息的控件
-        // normalMsgBadgeT.setBadgePosition(BadgeView.POSITION_RIGHT_CENTER);//
-        // 显示的位置.中间，还有其他位置属性
-        // normalMsgBadgeT.setTextColor(Color.WHITE); // 文本颜色
-        // normalMsgBadgeT.setBadgeBackgroundColor(getResources().getColor(R.color.text_red));
-        // // 背景颜色
-        // normalMsgBadgeT.setTextSize(getResources().getDimension(R.dimen.text_small));
-        //
-        // applicationMsgBadgeT = new BadgeView(getActivity(),
-        // applicationmsg_contentT);//
-        // 创建一个BadgeView对象，view为你需要显示提醒信息的控件
-        // applicationMsgBadgeT.setBadgePosition(BadgeView.POSITION_RIGHT_CENTER);//
-        // 显示的位置.中间，还有其他位置属性
-        // applicationMsgBadgeT.setTextColor(Color.WHITE); // 文本颜色
-        // applicationMsgBadgeT.setBadgeBackgroundColor(getResources().getColor(R.color.text_red));
-        // // 背景颜色
-        // applicationMsgBadgeT.setTextSize(getResources().getDimension(R.dimen.text_small));
         return view;
     }
 
@@ -155,17 +114,10 @@ public class MsgFragment extends Fragment {
             @Override
             public void doInUI(Response response, Integer transfer) {
                 if (response.isSuccess()) {
+                    mRefreshListView.onRefreshComplete();
+                    mRefreshListView.removeFootView();
                     JSONObject jo = response.jSONFromData();
-                    JSONObject commentJo = JSONUtil.getJSONObject(jo, "comment");
-                    normalMsgBadgeT.setVisibility(JSONUtil.getInt(commentJo, "count") == 0 ? View.GONE : View.VISIBLE);
-                    normsg_contentT.setText(JSONUtil.getString(commentJo, "content"));
-                    normalMsgBadgeT.setText(JSONUtil.getString(commentJo, "count"));
-
-                    JSONObject applicationJo = JSONUtil.getJSONObject(jo, "application");
-                    applicationMsgBadgeT.setVisibility(JSONUtil.getInt(applicationJo, "count") == 0 ? View.GONE
-                            : View.VISIBLE);
-                    applicationmsg_contentT.setText(JSONUtil.getString(applicationJo, "content"));
-                    applicationMsgBadgeT.setText(JSONUtil.getString(applicationJo, "count"));
+                    mAdapter.setData(jo);
                 }
             }
         });
---
+++ carplay/src/com/gongpingjia/carplay/adapter/FragmentMsgAdapter.java
@@ -0,0 +1,117 @@
+package com.gongpingjia.carplay.adapter;
+
+import net.duohuo.dhroid.net.DhNet;
+import net.duohuo.dhroid.net.JSONUtil;
+import net.duohuo.dhroid.net.NetTask;
+import net.duohuo.dhroid.net.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.gongpingjia.carplay.R;
+import com.gongpingjia.carplay.activity.msg.NewMessageActivity;
+import com.gongpingjia.carplay.api.API;
+import com.gongpingjia.carplay.bean.User;
+import com.gongpingjia.carplay.view.BadgeView;
+
+/*
+ *@author zhanglong
+ *Email:1269521147@qq.com
+ */
+public class FragmentMsgAdapter extends BaseAdapter {
+
+    private LayoutInflater mInflater;
+
+    private Context mContext;
+
+    BadgeView normalMsgBadgeT, applicationMsgBadgeT;
+
+    TextView applicationmsg_contentT, normsg_contentT;
+
+    View aaplication_layoutV, normsg_layoutV;
+    
+    JSONObject jo;
+
+    public FragmentMsgAdapter(Context context) {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return 1;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+         View view = mInflater.inflate(R.layout.information, null);
+        RelativeLayout leave_comments_layout = (RelativeLayout) view.findViewById(R.id.leave_comments_layout);
+        leave_comments_layout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(mContext, NewMessageActivity.class);
+                intent.putExtra("type", "comment");
+                mContext.startActivity(intent);
+
+            }
+        });
+        RelativeLayout apply_for_layout = (RelativeLayout) view.findViewById(R.id.apply_for_layout);
+        apply_for_layout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(mContext, NewMessageActivity.class);
+                intent.putExtra("type", "application");
+                mContext.startActivity(intent);
+            }
+        });
+        int bgcolor = mContext.getResources().getColor(R.color.text_orange);
+        applicationmsg_contentT = (TextView) view.findViewById(R.id.applicationmsg_content);
+        normsg_contentT = (TextView) view.findViewById(R.id.normsg_content);
+        normalMsgBadgeT = (BadgeView) view.findViewById(R.id.normal_msg_cout);
+        normalMsgBadgeT.setBadgeBackgroundColor(bgcolor);
+        applicationMsgBadgeT = (BadgeView) view.findViewById(R.id.application_msg_cout);
+        applicationMsgBadgeT.setBadgeBackgroundColor(bgcolor);
+        
+        if(jo!=null) {
+            JSONObject commentJo = JSONUtil.getJSONObject(jo, "comment");
+            normalMsgBadgeT.setVisibility(JSONUtil.getInt(commentJo, "count") == 0 ? View.GONE : View.VISIBLE);
+            normsg_contentT.setText(JSONUtil.getString(commentJo, "content"));
+            normalMsgBadgeT.setText(JSONUtil.getString(commentJo, "count"));
+
+            JSONObject applicationJo = JSONUtil.getJSONObject(jo, "application");
+            applicationMsgBadgeT.setVisibility(JSONUtil.getInt(applicationJo, "count") == 0 ? View.GONE
+                    : View.VISIBLE);
+            applicationmsg_contentT.setText(JSONUtil.getString(applicationJo, "content"));
+            applicationMsgBadgeT.setText(JSONUtil.getString(applicationJo, "count"));
+        }
+        
+        return view;
+    }
+    
+    public void setData (JSONObject jo) {
+        this.jo  =jo;
+        notifyDataSetChanged();
+    }
+
+}
