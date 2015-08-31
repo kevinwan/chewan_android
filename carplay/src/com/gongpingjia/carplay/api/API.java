36ee56a0109ee036182ca2b5c94ffcff2f6e6578
zlidentify <zlidentify@gmail.com>
2015-08-31 12:30:35 +0800

    update

--- carplay/src/com/gongpingjia/carplay/api/API.java
+++ carplay/src/com/gongpingjia/carplay/api/API.java
@@ -1,48 +1,51 @@
 package com.gongpingjia.carplay.api;
 
 public class API {
-	
-	public static final String update = "http://chewanapi.gongpingjia.com/version";
 
-	public static final String share = "http://chewanapi.gongpingjia.com";
+    public static final String update = "http://chewanapi.gongpingjia.com/version";
 
-	public static final String CWBaseurl = "http://chewanapi.gongpingjia.com/v1";
+    public static final String share = "http://chewanapi.gongpingjia.com";
 
-	public static final String allCarBrands = CWBaseurl + "/car/brand";
+    //正式版
+    // public static final String CWBaseurl =
+    // "http://chewanapi.gongpingjia.com/v1";
 
-	public static final String carDetails = CWBaseurl + "/car/model";
+    //测试版
+    public static final String CWBaseurl = "http://cwapi.gongpingjia.com/v1";
 
-	// 登录
-	public static final String login = CWBaseurl + "/user/login";
+    public static final String allCarBrands = CWBaseurl + "/car/brand";
 
-	// 重设密码
-	public static final String newPassword = CWBaseurl + "/user/password";
+    public static final String carDetails = CWBaseurl + "/car/model";
 
-	public static final String checkCode = CWBaseurl + "/phone/";
+    // 登录
+    public static final String login = CWBaseurl + "/user/login";
 
-	public static final String uploadHead = CWBaseurl + "/avatar/upload";
+    // 重设密码
+    public static final String newPassword = CWBaseurl + "/user/password";
 
-	public static final String register = CWBaseurl + "/user/register";
+    public static final String checkCode = CWBaseurl + "/phone/";
 
-	// 上传图片
-	public static final String uploadPictures = CWBaseurl
-			+ "/activity/cover/upload?";
+    public static final String uploadHead = CWBaseurl + "/avatar/upload";
 
-	public static final String activeList = CWBaseurl + "/activity/list";
+    public static final String register = CWBaseurl + "/user/register";
 
-	public static final String createActive = CWBaseurl + "/activity/register?";
+    // 上传图片
+    public static final String uploadPictures = CWBaseurl + "/activity/cover/upload?";
 
-	public static final String availableSeat = CWBaseurl + "/user/";
+    public static final String activeList = CWBaseurl + "/activity/list";
 
-	public static final String editActive = CWBaseurl + "/activity/";
+    public static final String createActive = CWBaseurl + "/activity/register?";
 
-	// 上传到相册
-	public static final String uploadAlbum = CWBaseurl + "/user/";
+    public static final String availableSeat = CWBaseurl + "/user/";
 
-	public static final String editAlbum = CWBaseurl + "/user/";
+    public static final String editActive = CWBaseurl + "/activity/";
 
+    // 上传到相册
+    public static final String uploadAlbum = CWBaseurl + "/user/";
 
-	// 官方活动
-	public static final String official = CWBaseurl + "/official/activity/list";
+    public static final String editAlbum = CWBaseurl + "/user/";
+
+    // 官方活动
+    public static final String official = CWBaseurl + "/official/activity/list";
 
 }
