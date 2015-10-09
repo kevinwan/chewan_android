4511a485c6bfe54afd88552440b48fc15e45fff9
hu845643018 <845643018@qq.com>
2015-10-09 11:17:23 +0800

    新增头像认证车主认证xml
    新增头像认证,车主认证xml

---
+++ carplay/src/com/gongpingjia/carplay/activity/my/AuthenticateOwnersActivity2.java
@@ -0,0 +1,20 @@
+package com.gongpingjia.carplay.activity.my;
+
+import android.os.Bundle;
+
+import com.gongpingjia.carplay.R;
+import com.gongpingjia.carplay.activity.CarPlayBaseActivity;
+
+public class AuthenticateOwnersActivity2 extends CarPlayBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authenticate_owners2);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
---
+++ carplay/src/com/gongpingjia/carplay/activity/my/HeadAttestationActivity.java
@@ -0,0 +1,20 @@
+package com.gongpingjia.carplay.activity.my;
+
+import android.os.Bundle;
+
+import com.gongpingjia.carplay.R;
+import com.gongpingjia.carplay.activity.CarPlayBaseActivity;
+
+public class HeadAttestationActivity extends CarPlayBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_head_attestation);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
