9ca2e257284321100a10e2e574858026d52c9458
hu845643018 <845643018@qq.com>
2015-10-09 09:01:27 +0800

    更新xml
    更新xml

---
+++ carplay/src/com/gongpingjia/carplay/activity/active/ActiveDetailsActivity2.java
@@ -0,0 +1,23 @@
+package com.gongpingjia.carplay.activity.active;
+
+import android.os.Bundle;
+
+import com.gongpingjia.carplay.R;
+import com.gongpingjia.carplay.activity.CarPlayBaseActivity;
+
+/**
+ * 活动详情
+ */
+public class ActiveDetailsActivity2 extends CarPlayBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_active_details2);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
