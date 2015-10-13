76cd51c3ddf46649e23c2e9543ab08bf344dee46
hu845643018 <845643018@qq.com>
2015-10-13 14:28:30 +0800

    更新代码
    更新代码

---
+++ carplay/src/com/gongpingjia/carplay/api/API2.java
@@ -0,0 +1,11 @@
+package com.gongpingjia.carplay.api;
+
+/**
+ * Created by Administrator on 2015/10/13.
+ */
+public class API2 {
+
+    //测试版API前缀
+    public static final String CWBaseurl = "http://cwapi.gongpingjia.com:8080/v2/";
+
+}
