6f2cf49f0786da1e02f2486ae337eacab048e65b
zlidentify <zlidentify@gmail.com>
2015-10-14 17:44:02 +0800

    login & register

--- carplay/src/com/gongpingjia/carplay/api/API2.java
+++ carplay/src/com/gongpingjia/carplay/api/API2.java
@@ -8,9 +8,20 @@ public class API2 {
     //测试版API前缀
     public static final String CWBaseurl = "http://cwapi.gongpingjia.com:8080/v2/";
 
-    public static final String getVarifacation = CWBaseurl + "phone/";
-
+    //获取验证码
+    public static final String getVerification = CWBaseurl + "phone/";
+    //验证码
     public static final String verifyCode = CWBaseurl + "phone/";
+    //手机号登陆
     public static final String login = CWBaseurl + "user/login";
+    //三方登陆
+    public static final String snsLogin = CWBaseurl + "sns/login";
+    //注册
+    public static final String register = CWBaseurl + "user/register";
+    //头像上传
+    public static final String uploadAvatar = CWBaseurl + "avatar/upload";
+
+    public static final String forgetPassword = CWBaseurl + "user/password";
+
 
 }
