340e8904d03231f795f98c4ed6d098a0a3271272
wangshuo5945 <308852453@qq.com>
2015-10-23 17:40:32 +0800

    提交代码
    提交代码

--- carplay/src/com/gongpingjia/carplay/activity/my/SubscribeEachOtherFragment.java
+++ carplay/src/com/gongpingjia/carplay/activity/my/SubscribeEachOtherFragment.java
@@ -7,7 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.gongpingjia.carplay.R;
 import com.gongpingjia.carplay.adapter.EachSubscribeAdapter2;
@@ -29,10 +31,11 @@ import org.json.JSONObject;
  * 互相关注
  */
 public class SubscribeEachOtherFragment extends Fragment {
-
+    View mView;
     private PullToRefreshListView mListView;
     private EachSubscribeAdapter2 mySubscriberAdapter;
-
+    LinearLayout empty;
+    TextView msg;
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
@@ -41,7 +44,10 @@ public class SubscribeEachOtherFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        mListView = (PullToRefreshListView) inflater.inflate(R.layout.fragment_follow_each_other, container, false);
+        mView = inflater.inflate(R.layout.fragment_follow_each_other, container, false);
+        mListView = (PullToRefreshListView)mView.findViewById(R.id.refresh_list_view);
+        empty = (LinearLayout) mView.findViewById(R.id.empty);
+        msg = (TextView) mView.findViewById(R.id.msg);
         mListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
             @Override
             public void onRefresh(PullToRefreshBase<ListView> refreshView) {
@@ -53,6 +59,10 @@ public class SubscribeEachOtherFragment extends Fragment {
                         if (response.isSuccess()) {
                             try {
                                 JSONObject jsonObject = response.jSONFromData();
+                                if (jsonObject != null){
+                                    empty.setVisibility(View.VISIBLE);
+                                    msg.setText("暂无关注");
+                                }
                                 JSONArray jsonArray = jsonObject.getJSONArray("eachSubscribe");
                                 mySubscriberAdapter = new EachSubscribeAdapter2(getActivity(), jsonArray);
                                 mListView.setAdapter(mySubscriberAdapter);
