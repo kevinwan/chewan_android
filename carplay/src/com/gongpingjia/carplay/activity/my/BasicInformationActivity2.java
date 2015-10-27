e76e1f586b30c5fc1b8d2fb8de730afe63ce18bc
zlidentify <zlidentify@gmail.com>
2015-10-27 11:01:26 +0800

    update

--- carplay/src/com/gongpingjia/carplay/activity/my/BasicInformationActivity2.java
+++ carplay/src/com/gongpingjia/carplay/activity/my/BasicInformationActivity2.java
@@ -4,17 +4,28 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.easemob.EMCallBack;
+import com.easemob.chat.EMChatManager;
+import com.easemob.chat.EMGroupManager;
 import com.gongpingjia.carplay.R;
 import com.gongpingjia.carplay.activity.CarPlayBaseActivity;
 import com.gongpingjia.carplay.activity.main.MainActivity2;
 import com.gongpingjia.carplay.api.API2;
 import com.gongpingjia.carplay.api.Constant;
+import com.gongpingjia.carplay.bean.LoginEB;
+import com.gongpingjia.carplay.bean.User;
+import com.gongpingjia.carplay.chat.DemoHXSDKHelper;
+import com.gongpingjia.carplay.chat.bean.ChatUser;
+import com.gongpingjia.carplay.chat.controller.HXSDKHelper;
+import com.gongpingjia.carplay.chat.db.UserDao;
 import com.gongpingjia.carplay.util.CarPlayPerference;
 import com.gongpingjia.carplay.util.MD5Util;
 import com.gongpingjia.carplay.view.dialog.DateTimerDialog2;
@@ -33,9 +44,13 @@ import net.duohuo.dhroid.util.UserLocation;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import de.greenrobot.event.EventBus;
 
 /**
  * Created by Administrator on 2015/10/13.
@@ -244,6 +259,133 @@ public class BasicInformationActivity2 extends CarPlayBaseActivity implements Vi
         });
     }
 
-    private void loginHX(String userId, String s, JSONObject jo) {
+    private void loginHX(String currentUsername, String currentPassword,
+                         final JSONObject jo) {
+        EMChatManager.getInstance().login(currentUsername, currentPassword,
+                new EMCallBack() {
+
+                    @Override
+                    public void onSuccess() {
+
+                        try {
+                            // ** 第一次登录或者之前logout后再登录，加载所有本地群和回话
+                            // ** manually load all local groups and
+                            EMGroupManager.getInstance().loadAllGroups();
+                            EMChatManager.getInstance().loadAllConversations();
+                            // 处理好友和群组
+                            initializeContacts();
+                            User user = User.getInstance();
+                            user.setToken(JSONUtil.getString(jo, "token"));
+                            user.setUserId(JSONUtil.getString(jo, "userId"));
+                            user.setNickName(mEditNickname.getText().toString());
+                            user.setHeadUrl(JSONUtil.getString(jo, "photo"));
+                            user.setLogin(true);
+
+                            LoginEB loginEB = new LoginEB();
+                            loginEB.setIslogin(true);
+                            EventBus.getDefault().post(loginEB);
+                            LoginActivity.asyncFetchGroupsFromServer();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            // 取好友或者群聊失败，不让进入主页面
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    DemoHXSDKHelper.getInstance().logout(true,
+                                            null);
+                                    Toast.makeText(getApplicationContext(),
+                                            R.string.login_failure_failed, 1)
+                                            .show();
+                                }
+                            });
+                            return;
+                        }
+                        // 更新当前用户的nickname 此方法的作用是在ios离线推送时能够显示用户nick
+                        boolean updatenick = EMChatManager.getInstance()
+                                .updateCurrentUserNick(
+                                        User.getInstance().getNickName());
+                        if (!updatenick) {
+                            Log.e("LoginActivity",
+                                    "update current user nick fail");
+                        }
+                        // if (!LoginActivity.this.isFinishing() &&
+                        // pd.isShowing()) {
+                        // pd.dismiss();
+                        // }
+                        // 进入主页面
+
+                    }
+
+                    @Override
+                    public void onProgress(int progress, String status) {
+                    }
+
+                    @Override
+                    public void onError(final int code, final String message) {
+                        // if (!progressShow) {
+                        // return;
+                        // }
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                // pd.dismiss();
+                                if (code == ERROR_EXCEPTION_INVALID_PASSWORD_USERNAME) {
+                                    showToast("用户名或者密码错误!");
+                                } else {
+                                    showToast("登录失败:" + message);
+                                }
+
+                            }
+                        });
+                    }
+                });
+
+    }
+
+    private void initializeContacts() {
+        Map<String, ChatUser> userlist = new HashMap<String, ChatUser>();
+        // 添加user"申请与通知"
+        ChatUser newFriends = new ChatUser();
+        newFriends
+                .setUsername(com.gongpingjia.carplay.chat.Constant.NEW_FRIENDS_USERNAME);
+        String strChat = getResources().getString(
+                R.string.Application_and_notify);
+        newFriends.setNick(strChat);
+
+        userlist.put(
+                com.gongpingjia.carplay.chat.Constant.NEW_FRIENDS_USERNAME,
+                newFriends);
+        // 添加"群聊"
+        ChatUser groupUser = new ChatUser();
+        String strGroup = getResources().getString(R.string.group_chat);
+        groupUser
+                .setUsername(com.gongpingjia.carplay.chat.Constant.GROUP_USERNAME);
+        groupUser.setNick(strGroup);
+        groupUser.setHeader("");
+        userlist.put(com.gongpingjia.carplay.chat.Constant.GROUP_USERNAME,
+                groupUser);
+
+        // 添加"Robot"
+        ChatUser robotUser = new ChatUser();
+        String strRobot = getResources().getString(R.string.robot_chat);
+        robotUser.setUsername(com.gongpingjia.carplay.chat.Constant.CHAT_ROBOT);
+        robotUser.setNick(strRobot);
+        robotUser.setHeader("");
+        userlist.put(com.gongpingjia.carplay.chat.Constant.CHAT_ROBOT,
+                robotUser);
+
+        // 存入内存
+        ((DemoHXSDKHelper) HXSDKHelper.getInstance()).setContactList(userlist);
+        // 存入db
+        UserDao dao = new UserDao(self);
+        List<ChatUser> users = new ArrayList<ChatUser>(userlist.values());
+        dao.saveContactList(users);
+    }
+
+    private String setData(int data) {
+        String strdata = String.valueOf(data);
+        if (strdata.length() == 1) {
+            return "0" + strdata;
+        }
+        return strdata;
+
     }
 }
