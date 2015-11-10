fad60e695a1a6a5040a9d1c609241044ceb3d451
qibutian <603986018@qq.com>
2015-11-10 19:14:04 +0800

    提交代码

--- carplay/src/com/gongpingjia/carplay/api/Constant.java
+++ carplay/src/com/gongpingjia/carplay/api/Constant.java
@@ -20,14 +20,14 @@ public class Constant {
 //	public static final String WX_APP_KEY = "wx9c80b151c6d4ae54";
 //	public static final String WX_APP_SECRET = "81077629b31790b0e8e8da336acdf4bb";
 
-    // 正式版
+    //     正式版
     public static final String WX_APP_KEY = "wx4c127cf07bd7d80b";
     //
     public static final String WX_APP_SECRET = "315ce754c5a1096c5188b4b69a7b9f04";
 
     // 车玩
-    // public static final String QQ_APP_ID = "1104728007";
-    // public static final String QQ_APP_KEY = "61BpHk8GQwH6FuCs";
+//     public static final String QQ_APP_ID = "1104728007";
+//     public static final String QQ_APP_KEY = "61BpHk8GQwH6FuCs";
 
     // umeng
     public static final String QQ_APP_ID = "100424468";
