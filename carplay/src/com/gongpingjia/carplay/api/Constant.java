69faaf6874b2920c437453bcc430a92f42c331ab
qibutian <603986018@qq.com>
2015-11-26 16:00:04 +0800

    提交代码

--- carplay/src/com/gongpingjia/carplay/api/Constant.java
+++ carplay/src/com/gongpingjia/carplay/api/Constant.java
@@ -21,20 +21,20 @@ public class Constant {
      */
     public static final int BLUR_VALUE = 6;
     // 测试版
-//	public static final String WX_APP_KEY = "wx9c80b151c6d4ae54";
-//	public static final String WX_APP_SECRET = "81077629b31790b0e8e8da336acdf4bb";
+    public static final String WX_APP_KEY = "wx9c80b151c6d4ae54";
+    public static final String WX_APP_SECRET = "81077629b31790b0e8e8da336acdf4bb";
 
     //     正式版
-    public static final String WX_APP_KEY = "wx4c127cf07bd7d80b";
-    //
-    public static final String WX_APP_SECRET = "315ce754c5a1096c5188b4b69a7b9f04";
+//    public static final String WX_APP_KEY = "wx4c127cf07bd7d80b";
+//    //
+//    public static final String WX_APP_SECRET = "315ce754c5a1096c5188b4b69a7b9f04";
 
     // 车玩 正式版
-    public static final String QQ_APP_ID = "1104728007";
-    public static final String QQ_APP_KEY = "61BpHk8GQwH6FuCs";
+//    public static final String QQ_APP_ID = "1104728007";
+//    public static final String QQ_APP_KEY = "61BpHk8GQwH6FuCs";
 
     // umeng
-//    public static final String QQ_APP_ID = "100424468";
-//
-//    public static final String QQ_APP_KEY = "c7394704798a158208a74ab60104f0ba";
+    public static final String QQ_APP_ID = "100424468";
+
+    public static final String QQ_APP_KEY = "c7394704798a158208a74ab60104f0ba";
 }
